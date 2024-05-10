8aa01130aa4d3d96cf66b6dd49b0bf7a0cf65ffc
Jinwoo Park <jinwoo.develop@gmail.com>
2024-05-10 15:11:18 +0900

    Modify commin initMap to async function

--- frontend/src/common/common.js
+++ frontend/src/common/common.js
@@ -1,12 +1,24 @@
-export function initMap(mapDiv){
-  const mapScript = document.createElement("script");
-  const clientId = import.meta.env.VITE_NAVER_API_KEY;
-  mapScript.src = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${clientId}`;
-  mapScript.async = true;
-  mapScript.defer = true;
-  document.head.appendChild(mapScript);
+const defaultLatLng = {
+  lat: 37.5004462,
+  lng: 127.037236,
+}
 
-  mapScript.onload = () => {
-    return new naver.maps.Map(mapDiv);
-  }
+export async function initMap(mapDiv, lat=defaultLatLng.lat, lng=defaultLatLng.lng){
+  return new Promise((resolve)=>{
+    const mapScript = document.createElement("script");
+    const clientId = import.meta.env.VITE_NAVER_API_KEY;
+    mapScript.src = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${clientId}`;
+    mapScript.async = true;
+    mapScript.defer = true;
+    document.head.appendChild(mapScript);
+  
+    mapScript.onload = () => {
+      console.log("LOAD")
+      const mapOptions = {
+          center: new naver.maps.LatLng(lat, lng),
+          zoom: 14
+      };
+      resolve(new naver.maps.Map(mapDiv, mapOptions));
+    }
+  })
 }
