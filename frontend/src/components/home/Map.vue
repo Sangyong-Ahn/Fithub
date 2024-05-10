76d6bafa2dd0387658d7d0d44aef61b2acf47239
Jinwoo Park <jinwoo.develop@gmail.com>
2024-05-10 16:10:24 +0900

    Sync with store

--- frontend/src/components/home/Map.vue
+++ frontend/src/components/home/Map.vue
@@ -5,120 +5,22 @@
 </template>
 
 <script setup>
+import { useProgramStore } from '@/stores/programStore';
 import { onMounted } from "vue";
 import * as common from "@/common/common.js";
 
-const programs = [ // sample data
-  {
-    "id": 2,
-    "mentorId": 1,
-    "categoryId": 3,
-    "title": "멘토 1의 글",
-    "reservationStartDate": "2024-05-09T15:00:00.000+00:00",
-    "reservationEndDate": "2024-05-09T15:00:00.000+00:00",
-    "programStartDate": "2024-05-09T15:00:00.000+00:00",
-    "programEndDate": "2024-05-09T15:00:00.000+00:00",
-    "thumbnail": "string",
-    "content": "string",
-    "youtubeUrl": "string",
-    "latitude": 37.5081110,
-    "longitude": 127.037237,
-    "createdAt": "2024-05-10T14:37:11.000+00:00",
-    "times": [
-      {
-        "id": 1,
-        "programId": 2,
-        "sunday": true,
-        "monday": false,
-        "tuesday": true,
-        "wednesday": false,
-        "thursday": false,
-        "friday": false,
-        "saturday": false,
-        "startTime": "12:00:00",
-        "endTime": "14:00:00",
-        "price": 23000,
-        "capacity": 5,
-        "reserveNum": 0
-      },
-      {
-        "id": 2,
-        "programId": 2,
-        "sunday": false,
-        "monday": true,
-        "tuesday": true,
-        "wednesday": false,
-        "thursday": false,
-        "friday": false,
-        "saturday": false,
-        "startTime": "14:00:00",
-        "endTime": "15:00:00",
-        "price": 15000,
-        "capacity": 3,
-        "reserveNum": 0
-      }
-    ],
-    "mentorInfo": null
-  },
-  {
-    "id": 3,
-    "mentorId": 2,
-    "categoryId": 3,
-    "title": "멘토 2의 글",
-    "reservationStartDate": "2024-05-09T15:00:00.000+00:00",
-    "reservationEndDate": "2024-05-09T15:00:00.000+00:00",
-    "programStartDate": "2024-05-09T15:00:00.000+00:00",
-    "programEndDate": "2024-05-09T15:00:00.000+00:00",
-    "thumbnail": "string",
-    "content": "string",
-    "youtubeUrl": "string",
-    "latitude": 37.5004463,
-    "longitude": 127.037236,
-    "createdAt": "2024-05-10T14:37:11.000+00:00",
-    "times": [
-      {
-        "id": 1,
-        "programId": 2,
-        "sunday": true,
-        "monday": false,
-        "tuesday": true,
-        "wednesday": false,
-        "thursday": false,
-        "friday": false,
-        "saturday": false,
-        "startTime": "12:00:00",
-        "endTime": "14:00:00",
-        "price": 3000,
-        "capacity": 5,
-        "reserveNum": 0
-      },
-      {
-        "id": 2,
-        "programId": 2,
-        "sunday": false,
-        "monday": true,
-        "tuesday": true,
-        "wednesday": false,
-        "thursday": false,
-        "friday": false,
-        "saturday": false,
-        "startTime": "14:00:00",
-        "endTime": "15:00:00",
-        "price": 7000,
-        "capacity": 3,
-        "reserveNum": 0
-      }
-    ],
-    "mentorInfo": null
-  }
-]
-let programId = null;
+const store = useProgramStore()
 
 onMounted(async ()=>{
     const mapDiv = document.getElementById("map");
     const map = await common.initMap(mapDiv);
-    
-    for(const program of programs){
+
+    if(!store.programList.value?.length){
+        alert("조건에 해당하는 프로그램이 없습니다.") // TODO: remove
+        store.programList.value = getTestProgramList();
+    }
+
+    for(const program of store.programList.value){
         const lowestPrice = common.getLowestPrice(program);
         const markerStyle = `
         background-color: white;
@@ -142,8 +44,116 @@ onMounted(async ()=>{
 
 function clickEvent(program){
     return function(e) {
-        console.log(program)
+        store.programList.value = [program]
     }
+}
+
+function getTestProgramList(){
+    return store.programList.value = // FOR TEST
+        [
+            {
+            "id": 2,
+            "mentorId": 1,
+            "categoryId": 3,
+            "title": "멘토 1의 글",
+            "reservationStartDate": "2024-05-09T15:00:00.000+00:00",
+            "reservationEndDate": "2024-05-09T15:00:00.000+00:00",
+            "programStartDate": "2024-05-09T15:00:00.000+00:00",
+            "programEndDate": "2024-05-09T15:00:00.000+00:00",
+            "thumbnail": "string",
+            "content": "string",
+            "youtubeUrl": "string",
+            "latitude": 37.5081110,
+            "longitude": 127.037237,
+            "createdAt": "2024-05-10T14:37:11.000+00:00",
+            "times": [
+                {
+                "id": 1,
+                "programId": 2,
+                "sunday": true,
+                "monday": false,
+                "tuesday": true,
+                "wednesday": false,
+                "thursday": false,
+                "friday": false,
+                "saturday": false,
+                "startTime": "12:00:00",
+                "endTime": "14:00:00",
+                "price": 23000,
+                "capacity": 5,
+                "reserveNum": 0
+                },
+                {
+                "id": 2,
+                "programId": 2,
+                "sunday": false,
+                "monday": true,
+                "tuesday": true,
+                "wednesday": false,
+                "thursday": false,
+                "friday": false,
+                "saturday": false,
+                "startTime": "14:00:00",
+                "endTime": "15:00:00",
+                "price": 15000,
+                "capacity": 3,
+                "reserveNum": 0
+                }
+            ],
+            "mentorInfo": null
+            },
+            {
+            "id": 3,
+            "mentorId": 2,
+            "categoryId": 3,
+            "title": "멘토 2의 글",
+            "reservationStartDate": "2024-05-09T15:00:00.000+00:00",
+            "reservationEndDate": "2024-05-09T15:00:00.000+00:00",
+            "programStartDate": "2024-05-09T15:00:00.000+00:00",
+            "programEndDate": "2024-05-09T15:00:00.000+00:00",
+            "thumbnail": "string",
+            "content": "string",
+            "youtubeUrl": "string",
+            "latitude": 37.5004463,
+            "longitude": 127.037236,
+            "createdAt": "2024-05-10T14:37:11.000+00:00",
+            "times": [
+                {
+                "id": 1,
+                "programId": 2,
+                "sunday": true,
+                "monday": false,
+                "tuesday": true,
+                "wednesday": false,
+                "thursday": false,
+                "friday": false,
+                "saturday": false,
+                "startTime": "12:00:00",
+                "endTime": "14:00:00",
+                "price": 3000,
+                "capacity": 5,
+                "reserveNum": 0
+                },
+                {
+                "id": 2,
+                "programId": 2,
+                "sunday": false,
+                "monday": true,
+                "tuesday": true,
+                "wednesday": false,
+                "thursday": false,
+                "friday": false,
+                "saturday": false,
+                "startTime": "14:00:00",
+                "endTime": "15:00:00",
+                "price": 7000,
+                "capacity": 3,
+                "reserveNum": 0
+                }
+            ],
+            "mentorInfo": null
+            }
+        ]
 }
 
 </script>
